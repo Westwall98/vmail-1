4ea135fb7f8e6dd1f26afea3e1bd863420f7f2f8
Ethan <35027934+Westwall98@users.noreply.github.com>
2024-03-21 22:52:00 +0800

    Update Header.tsx

--- apps/remix/app/components/Header.tsx
+++ apps/remix/app/components/Header.tsx
@@ -7,9 +7,10 @@ export default function Header() {
   const { t } = useTranslation();
   return (
     <div className="fixed top-0 z-20 h-20 w-full px-5 backdrop-blur-xl md:px-10 text-white flex items-center justify-between first-letter:shadow-sm">
-      <button className="cool-btn">
-        <span>WESTWALL</span>
-      </button>
+        <button className="cool-btn">
+          <span>WESTWALL</span>
+        </button>
+      </Link>
     </div>
   );
 }

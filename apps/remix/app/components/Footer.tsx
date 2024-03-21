2c281d8690137953bfde146106b8b5432c4e2aa8
Ethan <35027934+Westwall98@users.noreply.github.com>
2024-03-21 22:39:53 +0800

    Update Footer.tsx

--- apps/remix/app/components/Footer.tsx
+++ apps/remix/app/components/Footer.tsx
@@ -1,17 +1,12 @@
-import { Link } from "@remix-run/react";
-import Twitter from "./icons/Twitter";
-import Github from "./icons/GitHub";
-import { MailIcon } from "icons";
-import Coffee from "./icons/Coffee";
 import { useTranslation } from "react-i18next";
 
 export default function Footer() {
   const { t } = useTranslation();
   return (
     <div className="text-white w-full mt-auto flex flex-col items-center justify-between px-5 pt-16 mb-10 md:px-10 mx-auto sm:flex-row">
-        <button className="cool-btn">
+      <button className="cool-btn">
           <span>WESTWALL</span>
-        </button>
+      </button>
       <Link
         className="mt-4 text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
         to="https://vmail.westwall.eu.org"

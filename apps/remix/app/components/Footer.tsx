1d1dd7ccab42b47258b3bf26cd1e7870509ab898
Ethan <35027934+Westwall98@users.noreply.github.com>
2024-03-21 11:29:21 +0800

    Update Footer.tsx

--- apps/remix/app/components/Footer.tsx
+++ apps/remix/app/components/Footer.tsx
@@ -25,37 +25,10 @@ export default function Footer() {
       </div>
       <div className="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
         <Link
-          to="https://vmail.westwall.eu.org"
-          target="_blank"
-          title="Vmail"
-          className="text-gray-400 hover:text-gray-500  scale-[1.2]">
-          🌍
-        </Link>
-        <Link
           to="mailto:ethan@westwall.vip"
           title="Email"
           className="text-gray-400 hover:text-gray-500">
           <MailIcon className="w-6 h-6" />
-        </Link>
-        <Link
-          to="https://vmail.westwall.eu.org"
-          target="_blank"
-          title="Twitter"
-          className="text-gray-400 hover:text-gray-500">
-          <Twitter />
-        </Link>
-        <Link
-          to="https://github.com/Westwall98/vmail"
-          target="_blank"
-          title="Github"
-          className="text-gray-400 hover:text-gray-500">
-          <Github />
-        </Link>
-        <Link
-          to="https://www.buymeacoffee.com/westwall"
-          target="_blank"
-          title="Buy me a coffee">
-          <Coffee className="w-6 h-6" />
         </Link>
       </div>
     </div>

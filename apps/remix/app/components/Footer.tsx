79558b8cff30ea2a14784feb1c6e7fd3fc470094
Ethan <35027934+Westwall98@users.noreply.github.com>
2024-03-21 22:49:24 +0800

    Update Footer.tsx

--- apps/remix/app/components/Footer.tsx
+++ apps/remix/app/components/Footer.tsx
@@ -8,17 +8,5 @@ import { useTranslation } from "react-i18next";
 export default function Footer() {
   const { t } = useTranslation();
   return (
-    <div className="text-white w-full mt-auto flex flex-col items-center justify-between px-5 pt-16 mb-10 md:px-10 mx-auto sm:flex-row">
-      <button className="cool-btn">
-          <span>WESTWALL</span>
-      </button>
-      <Link
-        className="mt-4 text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        to="https://vmail.westwall.eu.org"
-        target="_blank">
-        {" "}
-        © 2024 Products
-      </Link>
-    </div>
   );
 }

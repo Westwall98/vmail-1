a39c1eefce809e8fa0f14ba233add70c09cbec4b
Ethan <35027934+Westwall98@users.noreply.github.com>
2024-03-23 14:11:19 +0800

    Update Footer.tsx

--- apps/remix/app/components/Footer.tsx
+++ apps/remix/app/components/Footer.tsx
@@ -1,8 +1,5 @@
 import { Link } from "@remix-run/react";
-import Twitter from "./icons/Twitter";
-import Github from "./icons/GitHub";
 import { MailIcon } from "icons";
-import Coffee from "./icons/Coffee";
 import { useTranslation } from "react-i18next";
 
 export default function Footer() {

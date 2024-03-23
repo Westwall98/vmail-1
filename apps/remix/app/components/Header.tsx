835e97a7f8618722a0cacb31be530a8a03f45b7a
Ethan <35027934+Westwall98@users.noreply.github.com>
2024-03-23 14:10:49 +0800

    Update Header.tsx

--- apps/remix/app/components/Header.tsx
+++ apps/remix/app/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@remix-run/react";
-import { VmailLogo } from "./icons/vmail";
+
 import { useTranslation } from "react-i18next";
-import Follow from "./icons/Follow";
+
 
 export default function Header() {
   const { t } = useTranslation();
